8334f5f7e8e5bc3bc0c2e912fffdfa0034b9556a
Oliver <rosszbikalo@gmail.com>
2024-11-03 16:35:44 +0100

    Initial commit

--- migrations/0004_create_user_books.sql
+++ migrations/0004_create_user_books.sql
@@ -1,11 +1,19 @@
-CREATE TABLE IF NOT EXISTS `user_books` (
+CREATE TABLE IF NOT EXISTS `purchase_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `bookId` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'Processing',
+  `price` int(5) NOT NULL,
+  `purchaseDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
+  CONSTRAINT `user_books_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `purchased_books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookId` int(11) NOT NULL,
+  `price` int(5) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `bookId` (`bookId`),
-  CONSTRAINT `user_books_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
-  CONSTRAINT `user_books_ibfk_2` FOREIGN KEY (`bookId`) REFERENCES `books` (`id`)
+  CONSTRAINT `purchase_history_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `books` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
