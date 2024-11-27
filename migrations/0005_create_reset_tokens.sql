0532d9a054e4db8a7b9ff7022c8673561c50e4a3
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-11-27 19:54:29 +0100

    Remove Reset token table

--- migrations/0005_create_reset_tokens.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `reset_tokens` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `userId` int(11) NOT NULL,
-    `token` varchar(255) NOT NULL,
-    `tokenExpires` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE
-);
